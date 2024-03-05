6e275090ebf0ef5d4feabc92212adaa1d0b04d3f
Mathew <muckele7@gmail.com>
2024-03-05 14:43:58 -0500

    creates home index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+HOME_TYPES = (
+  ('house', 'House'),
+  ('lot', 'Lot'),
+  ('multifamily', 'Multi-family'),
+  ('condo', 'Condominium'),
+  ('townhomes', 'Townhomes'),
+)
+
+
 # Create your models here.
+class Home(models.Model):
+  price = models.IntegerField()
+  address = models.CharField(max_length=255)
+  bedrooms = models.IntegerField()
+  bathrooms = models.DecimalField(max_digits=3, decimal_places=1)
+  sqft = models.IntegerField()
+  description = models.TextField()
+  lotsize = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Lot Size")
+  tax_assessment = models.IntegerField()
+  tax_year = models.IntegerField()
+  listed_by = models.CharField(max_length=255)
+  type = models.CharField(max_length=50, choices=HOME_TYPES, default=HOME_TYPES[0][0])
+
+  def __str__(self):
+    return self.address
+  
